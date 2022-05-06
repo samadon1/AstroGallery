d48e713f4441321559b6a026ce3d77c169b64250
Samuel Amankwah Donkor <56901167+samadon1@users.noreply.github.com>
2022-05-06 00:43:06 +0000

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import ImageCard from "./components/imageCard";
 import ImageSearch from "./components/imageSearch";
-import {motion} from 'framer-motion'
+import { motion } from "framer-motion";
 
 export default function App() {
   const [images, setImages] = useState([]);
@@ -20,31 +20,36 @@ export default function App() {
       .catch((err) => console.log(err));
   }, [term]);
 
-  return ( 
-    
-    <motion.div   
-    initial={{ y:520 }}
-    animate={{ y:0 , marginTop: 120,}}
-    transition={{ type: "spring", stiffness: 40}}
-    className=" container mx-auto">
+  return (
+    <motion.div
+      initial={{ y: 520 }}
+      animate={{ y: 0, marginTop: 120 }}
+      transition={{ type: "spring", stiffness: 40 }}
+      className=" container mx-auto"
+    >
       <div className="flex justify-center text-5xl font-bold ">
-      <motion.h1  
-      initial={{opacity: 0}}
-      animate={{scale:2, marginBottom: 80, marginTop: 120, opacity: 0.8 }}
-      transition={{delay: 1.2}}
-      >
-      AstroGallery  
-      </motion.h1>
+        <motion.h1
+          initial={{ opacity: 0 }}
+          animate={{ scale: 2, marginBottom: 80, marginTop: 120, opacity: 0.8 }}
+          transition={{ delay: 1.2 }}
+        >
+          AstroGallery
+        </motion.h1>
       </div>
-      
+
       <ImageSearch searchText={(text) => setTerm(text)} />
-      {!isLoading && images.length === 0 && <h1 className="text-6xl text-center mx-auto mt-32">No Images Found</h1>}
-    {isLoading ? <h1 className="text-3xl text-center mx-auto mt-32">Loading...</h1> 
-    : <div className="grid grid-cols-3 gap-4">
-      {images.map(image =>(
-        <ImageCard key={image. id} image={image}/>
-      ))}
-    </div>}
-  </motion.div>
+      {!isLoading && images.length === 0 && (
+        <h1 className="text-6xl text-center mx-auto mt-32">No Images Found</h1>
+      )}
+      {isLoading ? (
+        <h1 className="text-3xl text-center mx-auto mt-32">Loading...</h1>
+      ) : (
+        <div className="grid grid-cols-3 gap-4">
+          {images.map((image) => (
+            <ImageCard key={image.id} image={image} />
+          ))}
+        </div>
+      )}
+    </motion.div>
   );
 }
--- src/components/imageCard.js
+++ src/components/imageCard.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function ImageCard(props) {
-    const tags = props.image.tags.split();
+    // const tags = props.image.tags.split();
 
     
   return (
