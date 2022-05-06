85df3aa564e78a5c2e1f5ba934c89b58a4d3abcb
Samuel Amankwah Donkor <56901167+samadon1@users.noreply.github.com>
2022-05-06 00:54:40 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ export default function App() {
 
   useEffect(() => {
     fetch(
-      `https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo&pretty=true`
+      `https://pixabay.com/api/?key=22051326-66bc0f38bcfa6c1926468b752&q=${term}&image_type=photo&pretty=true`
     )
       .then((res) => res.json())
       .then((data) => {
